d8e67732db6aded844b8200d5d0fbe05667a2b48
cwaldburger <cedric.waldburger@gmail.com>
2013-07-15 16:16:54 +0200

    commiting changes before switching to a programm branch

---
+++ snf/snfInfoPlanViewController.h
@@ -0,0 +1,15 @@
+//
+//  snfInfoPlanViewController.h
+//  snf
+//
+//  Created by Cédric Waldburger on 7/15/13.
+//  Copyright (c) 2013 mediasign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface snfInfoPlanViewController : UIViewController <UIScrollViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+
+@end
