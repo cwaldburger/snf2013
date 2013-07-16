30dc6768b86f8e81f745b0e5d1b4a056af45c90a
cwaldburger <cedric.waldburger@gmail.com>
2013-07-16 04:06:41 +0200

    basic tableview working

---
+++ snfFestbeizenViewController.h
@@ -0,0 +1,13 @@
+//
+//  snfFestbeizenViewController.h
+//  snf
+//
+//  Created by Cédric Waldburger on 7/16/13.
+//  Copyright (c) 2013 mediasign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface snfFestbeizenViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ snfFestbeizenViewController.m
@@ -0,0 +1,62 @@
+//
+//  snfFestbeizenViewController.m
+//  snf
+//
+//  Created by Cédric Waldburger on 7/16/13.
+//  Copyright (c) 2013 mediasign. All rights reserved.
+//
+
+#import "snfFestbeizenViewController.h"
+
+@interface snfFestbeizenViewController ()
+
+@end
+
+@implementation snfFestbeizenViewController
+{
+    NSArray *tableData;
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    // Initialize table data
+    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+
+@end
