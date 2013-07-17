ef223fe13aeb8c926adca8b85aa15c5cfbb0d281
cwaldburger <cedric.waldburger@gmail.com>
2013-07-17 18:58:40 +0200

    added modal view, adding new classes for modal view

---
+++ snf/snfFestbeizenDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  snfFestbeizenDetailViewController.h
+//  snf
+//
+//  Created by Cédric Waldburger on 7/17/13.
+//  Copyright (c) 2013 mediasign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface snfFestbeizenDetailViewController : UIViewController
+
+@end
