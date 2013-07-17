1def55ee3b1923d85f9f373df534d700ba22de49
cwaldburger <cedric.waldburger@gmail.com>
2013-07-17 21:06:21 +0200

    statisches label funktioniert

--- snf/snfFestbeizenDetailViewController.m
+++ snf/snfFestbeizenDetailViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "snfFestbeizenDetailViewController.h"
+#import "FestbeizLocation.h"
 
 #define METERS_PER_MILE 1609.344
 
@@ -44,6 +45,39 @@
 	[self.delegate snfFestbeizenDetailViewControllerDidClose:self];
 }
 
+
+// Add new method above refreshTapped
+- (void)plotCrimePositions:(NSData *)responseData {
+//    for (id<MKAnnotation> annotation in festbeizenMapView.annotations) {
+//        [festbeizenMapView removeAnnotation:annotation];
+//    }
+    
+//    NSDictionary *root = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
+//    NSArray *data = [root objectForKey:@"data"];
+    
+//    for (NSArray *row in data) {
+//        NSNumber * latitude = [[row objectAtIndex:22]objectAtIndex:1];
+//        NSNumber * longitude = [[row objectAtIndex:22]objectAtIndex:2];
+//        NSString * crimeDescription = [row objectAtIndex:18];
+//        NSString * address = [row objectAtIndex:14];
+//        
+//        CLLocationCoordinate2D coordinate;
+//        coordinate.latitude = latitude.doubleValue;
+//        coordinate.longitude = longitude.doubleValue;
+//        MyLocation *annotation = [[MyLocation alloc] initWithName:crimeDescription address:address coordinate:coordinate] ;
+//        [_mapView addAnnotation:annotation];
+//	}
+    
+    CLLocationCoordinate2D coordinate;
+    coordinate.latitude = 47.227048;
+    coordinate.longitude = 8.814532;
+    NSString * address = @"Haldenstrasse 13";
+    FestbeizLocation *annotation = [[FestbeizLocation alloc] initWithName:@"Test" address:address coordinate:coordinate] ;
+    [festbeizenMapView addAnnotation:annotation];
+    
+}
+
+
 - (void)viewWillAppear:(BOOL)animated {
     // 1
     CLLocationCoordinate2D zoomLocation;
@@ -55,6 +89,10 @@
     
     // 3
     [festbeizenMapView setRegion:viewRegion animated:YES];
+    
+    // Add new line inside refreshTapped, in the setCompletionBlock, right after logging the response string
+    [self plotCrimePositions:nil];
 }
+
 
 @end
