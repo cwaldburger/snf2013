a9571045c7fa02cdeec71a82e424de875baf59c2
cwaldburger <cedric.waldburger@gmail.com>
2013-07-20 11:27:15 +0100

    preparing new build

--- snf/snfProgrammViewController.m
+++ snf/snfProgrammViewController.m
@@ -45,31 +45,58 @@
 - (IBAction)segmentedControlChanged:(UISegmentedControl *)sender {
 
     NSString *tagID = nil;
+    NSString *path = nil;
+    NSString *newText = nil;
     
     switch (sender.selectedSegmentIndex) {
             //            freitag
         case 0:
             tagID = @"freitagText";
+            path = [[NSBundle mainBundle] pathForResource:tagID ofType:@"txt"];
+            newText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+            
+//            NSMutableAttributedString *infoString = [[NSMutableAttributedString alloc]
+//                                                     initWithString: newText];
+//            
+//            [infoString addAttribute:NSFontAttributeName
+//                               value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:14.0]
+//                               range:NSMakeRange(896,9)];
+//            
+//            [infoString addAttribute:NSFontAttributeName
+//                               value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:14.0]
+//                               range:NSMakeRange(1068,7)];
+//            
+//            [infoString addAttribute:NSFontAttributeName
+//                               value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:14.0]
+//                               range:NSMakeRange(1320,13)];
+//            
+//            programmTextView.attributedText = infoString;
+            
             break;
             //            samstag
         case 1:
             tagID = @"samstagText";
+            path = [[NSBundle mainBundle] pathForResource:tagID ofType:@"txt"];
+            newText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
             break;
             //            sonntag
         case 2:
             tagID = @"sonntagText";
+            path = [[NSBundle mainBundle] pathForResource:tagID ofType:@"txt"];
+            newText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
             break;
             //            kinder
         case 3:
             tagID = @"kinderText";
+            path = [[NSBundle mainBundle] pathForResource:tagID ofType:@"txt"];
+            newText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
             break;
      
         default:
             break;
     }
     
-    NSString *path = [[NSBundle mainBundle] pathForResource:tagID ofType:@"txt"];
-    NSString *newText = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+
     programmTextView.text = newText;
     [self.programmTextView setContentOffset:CGPointZero animated:NO];
     
